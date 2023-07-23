e9f96b738ea9032e4e2a0112b5fb7b9ebab0b3a5
Konrad Hoerner <konradh1997@gmail.com>
2023-07-23 17:28:31 +0200

    signin/out and current-user handling added

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -4,44 +4,56 @@ import { Password } from '../services/password';
 // An interface that describes the properties that
 // are required to create a new User
 interface UserAttributes {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 // An interface that describes the properties that
 // that a User Model has
 interface UserModel extends mongoose.Model<UserDoc> {
-	build(attributes: UserAttributes): UserDoc;
+  build(attributes: UserAttributes): UserDoc;
 }
 
 // An interface that describes the properties that
 // that user Document has
 interface UserDoc extends mongoose.Document {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
-const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.password;
+      },
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
-	if (this.isModified('password')) {
-		const hashed = await Password.toHash(this.get('password'));
-		this.set('password', hashed);
-	}
-	done();
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attributes: UserAttributes) => {
-	return new User(attributes);
+  return new User(attributes);
 };
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,56 +1,51 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
-	'/api/users/signup',
-	[
-		body('email').isEmail().withMessage('Email must be valid!'),
-		body('password')
-			.trim()
-			.isLength({ min: 4, max: 20 })
-			.withMessage('Password must be between 4 and 20 characters!'),
-	],
-	async (req: Request, res: Response) => {
-		const errors = validationResult(req);
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters!'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-		if (!errors.isEmpty()) {
-			throw new RequestValidationError(errors.array());
-		}
+    const existingUser = await User.findOne({ email });
 
-		const { email, password } = req.body;
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
-		const existingUser = await User.findOne({ email });
+    const user = User.build({ email, password });
+    await user.save();
 
-		if (existingUser) {
-			throw new BadRequestError('Email in use');
-		}
+    // Generatr JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
 
-		const user = User.build({ email, password });
-		await user.save();
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
 
-		// Generatr JWT
-		const userJwt = jwt.sign(
-			{
-				id: user.id,
-				email: user.email,
-			},
-			process.env.JWT_KEY!
-		);
-
-		// Store it on session object
-		req.session = {
-			jwt: userJwt,
-		};
-
-		res.status(201).send(user);
-	}
+    res.status(201).send(user);
+  }
 );
 
 export { router as signupRouter };
