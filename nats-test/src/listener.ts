ad5486638aeac4f21d917f9e5bd1f0e99bffdfb2
Konrad <konradh1997@gmail.com>
2023-08-05 19:37:35 +0200

    creating NATS service

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,37 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+	url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+	console.log('Listener connected to NATS');
+
+	stan.on('close', () => {
+		console.log('NATS connection closed!');
+		process.exit();
+	});
+
+	const options = stan.subscriptionOptions().setManualAckMode(true);
+	const subscirption = stan.subscribe(
+		'ticket:created',
+		'orders-service-queue-group',
+		options
+	);
+
+	subscirption.on('message', (msg: Message) => {
+		const data = msg.getData();
+
+		if (typeof data === 'string') {
+			console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
+		}
+
+		msg.ack();
+	});
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
